731883232db0a72f0d255f2bf3cde9e6195a3759
tanu <dastanushri402@gmail.com>
2023-06-02 19:55:52 +0600

    nine

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -4,13 +4,12 @@ import Home from "../../Pages/Home/Home";
 import ShowDetails from "../../Pages/ShowDetails/ShowDetails";
 import About from "../../Pages/About/About";
 import Contact from "../../Pages/Contact/Contact";
-import Error from "../../Pages/Error/Error";
+
 
 const routes=createBrowserRouter([
     {
         path:'/',
         element:<Main></Main>,
-        errorElement:<Error/>,
         children:[
             {
                 path:'/',
