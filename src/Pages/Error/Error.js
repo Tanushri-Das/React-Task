c9dba7b5062db0b72bee778b26f581c1f80912d5
tanu <dastanushri402@gmail.com>
2023-06-02 19:50:32 +0600

    eight

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -4,14 +4,11 @@ import Image from 'react-bootstrap/Image'
 
 const Error = () => {
   return (
-    <Container className="mt-5">
+    <Container className="mt-5 d-flex align-items-center justify-content-center">
     <Row>
-        <div className="col-lg-3"></div>
-        <div className='col-lg-6'>
-            
-            <Image src="https://internetdevels.com/sites/default/files/public/blog_preview/404_page_cover.jpg" fluid/>
-        </div>
-        <div className="col-lg-3"></div>
+      <div className="col-lg-6">
+        <Image src="https://internetdevels.com/sites/default/files/public/blog_preview/404_page_cover.jpg" fluid />
+      </div>
     </Row>
   </Container>
   )
